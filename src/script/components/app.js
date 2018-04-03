1e1ac8b897e715fe6291247f56537a4a241e62b7
Ruben Medina <dev@rubenmedina.com>
2018-04-02 23:30:09 -0700

    Optimize webpack configuration. Add chunked bundles including a runtime chunk, hash module ids, change the way favicon is loaded, generate manifest file

--- src/script/components/app.js
+++ src/script/components/app.js
@@ -4,11 +4,6 @@ import LeftColumn from './leftColumn';
 import Main from './main';
 import RightColumn from './rightColumn';
 import Footer from './footer';
-import FavIcon from './../../images/favicon.ico';
-
-if (module.hot) {
-    require('preact/debug');
-}
 
 export default class App extends Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const InlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const webpack = require('webpack');
 
 // https://github.com/webpack-contrib/mini-css-extract-plugin
 
@@ -50,33 +53,28 @@ module.exports = (env, argv) => {
                     },
                 },
                 {
-                    test: /\.(png|svg|jpg|gif)$/,
-                    use: [
-                        {
-                            loader: 'url-loader',
-                            options: {
-                                name: 'images/[name].[ext]',
-                                limit: 10000,
-                            },
-                        },
-                        {
-                            loader: 'image-webpack-loader',
-                            options: {
-                                bypassOnDebug: true,
-                            },
-                        },
-                    ],
+                    test: /\.svg$/,
+                    loader: 'svg-url-loader',
+                    options: {
+                        name: 'images/[hash].[ext]',
+                        limit: 1024 * 1,
+                    },
                 },
                 {
-                    test: /\.(ico)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: 'images/[name].[ext]',
-                            },
-                        },
-                    ],
+                    test: /\.(png|jpe?g|gif)$/,
+                    loader: 'url-loader',
+                    options: {
+                        name: 'images/[hash].[ext]',
+                        limit: 1024 * 1,
+                    },
+                },
+                {
+                    test: /\.(png|svg|jpe?g|gif)$/,
+                    loader: 'image-webpack-loader',
+                    options: {
+                        bypassOnDebug: true,
+                    },
+                    enforce: 'pre',
                 },
                 {
                     test: /\.js$/,
@@ -108,17 +106,27 @@ module.exports = (env, argv) => {
             new HtmlWebpackPlugin({
                 template: 'src/index.html',
                 inject: true,
-                hash: true,
+                favicon: 'src/favicon.ico',
+                inlineSource: 'runtime~.+\\.js',
             }),
+            new InlineSourcePlugin(),
             extractSass,
             new ProgressBarPlugin({
                 format: '\u001b[90m\u001b[44mBuild\u001b[49m\u001b[39m [:bar] \u001b[32m\u001b[1m:percent\u001b[22m\u001b[39m (:elapseds) \u001b[2m:msg\u001b[22m',
                 renderThrottle: 100,
             }),
+            new webpack.HashedModuleIdsPlugin(),
+            new ManifestPlugin(),
         ],
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+            },
+            runtimeChunk: true,
+        },
         devtool: isProd ? 'nosources-source-map' : 'eval-source-map',
         output: {
-            filename: 'scripts/[name].bundle.js',
+            filename: 'scripts/[name].[chunkhash].js',
             path: path.resolve(__dirname, 'dist'),
         },
     };
